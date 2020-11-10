5431dc9da276f5e3b8faebc7d9d85049e385d89b
Benjamin Hills <bhills@uw.edu>
2020-11-10 13:15:21 -0700

    Small changes to the multi-beam plotting script
    I wanted to remove the dependency on the MOA file.

--- IS2_velocity/plotting.py
+++ IS2_velocity/plotting.py
@@ -10,7 +10,7 @@ except:
 import matplotlib.pyplot as plt
 
 def plot_measures_along_track_comparison(rgt, beams, out_path, correlation_threshold, spatial_extent, plot_out_location, map_data_root,
-                                         velocity_number, close=False):
+                                         velocity_number, epsg, close=False):
     """
 
     :param rgt:
@@ -27,33 +27,36 @@ def plot_measures_along_track_comparison(rgt, beams, out_path, correlation_thres
     # plot_out_location is where to save the plot
     # map_data_root is where the map data are stored, specifically must contain moa_2009_1km.tif for this specific code to work
 
-    file = out_path + 'rgt' + rgt + '_veloc' + str(velocity_number).zfill(
-        2) + '.hdf5'  # < eventually, make velocity number two digits
+    file = out_path + 'rgt' + rgt +  '.hdf5'
+    # < eventually, make velocity number two digits
+    # '_veloc' + str(velocity_number).zfill(2) +
 
     if glob.glob(file):
-
-        ### MOA parameters
-        moa_datapath = map_data_root  # '/srv/tutorial-data/land_ice_applications/'
-        # spatial_extent = np.array([-102, -76, -98, -74.5])
-        # spatial_extent = np.array([-65, -86, -55, -81])
-
-        lat = spatial_extent[[1, 3, 3, 1, 1]]
-        lon = spatial_extent[[2, 2, 0, 0, 2]]
-        # project the coordinates to Antarctic polar stereographic
-        xy = np.array(pyproj.Proj(3031)(lon, lat))
-        # get the bounds of the projected coordinates
-        XR = [np.nanmin(xy[0, :]), np.nanmax(xy[0, :])]
-        YR = [np.nanmin(xy[1, :]), np.nanmax(xy[1, :])]
-        MOA = pc.grid.data().from_geotif(os.path.join(moa_datapath, 'moa_2009_1km.tif'), bounds=[XR, YR])
-        #             MOA=pc.grid.data().from_geotif(os.path.join(moa_datapath, 'MOA','moa_2009_1km.tif'), bounds=[XR, YR])
-
-        epsg = 3031  # PS?
 
         plt.close('all')
         fig = plt.figure(figsize=[11, 8])
         grid = plt.GridSpec(6, 2, wspace=0.4, hspace=0.3)
-        haxMOA = fig.add_subplot(grid[0:4, 1])
-        MOA.show(ax=haxMOA, cmap='gray', clim=[14000, 17000])
+
+        try:
+            ### MOA parameters
+            moa_datapath = map_data_root  # '/srv/tutorial-data/land_ice_applications/'
+
+            lat = spatial_extent[[1, 3, 3, 1, 1]]
+            lon = spatial_extent[[2, 2, 0, 0, 2]]
+
+            # project the coordinates to Antarctic polar stereographic
+            xy = np.array(pyproj.Proj(epsg)(lon, lat))
+            # get the bounds of the projected coordinates
+            XR = [np.nanmin(xy[0, :]), np.nanmax(xy[0, :])]
+            YR = [np.nanmin(xy[1, :]), np.nanmax(xy[1, :])]
+            #             MOA=pc.grid.data().from_geotif(os.path.join(moa_datapath, 'MOA','moa_2009_1km.tif'), bounds=[XR, YR])
+
+            MOA = pc.grid.data().from_geotif(os.path.join(moa_datapath, 'moa_2009_1km.tif'), bounds=[XR, YR])
+
+            haxMOA = fig.add_subplot(grid[0:4, 1])
+            MOA.show(ax=haxMOA, cmap='gray', clim=[14000, 17000])
+        except:
+            pass
 
         with h5py.File(file, 'r') as f:
             for ib, beam in enumerate(beams):
@@ -81,11 +84,13 @@ def plot_measures_along_track_comparison(rgt, beams, out_path, correlation_thres
                 c = plt.colorbar(h0, ax=hax0)
                 c.set_label('Correlation coefficient (0 -> 1)')
 
-                h2 = haxMOA.scatter(xy[0][ixs0], xy[1][ixs0], 0.02, 'k')
-                h3 = haxMOA.scatter(xy[0][ixs], xy[1][ixs], 0.15, velocs[ixs], vmin=-800, vmax=800, cmap='plasma')
-
-        c = plt.colorbar(h3, ax=haxMOA)
-        c.set_label('Along-track velocity (m/yr)')
+                try:
+                    h2 = haxMOA.scatter(xy[0][ixs0], xy[1][ixs0], 0.02, 'k')
+                    h3 = haxMOA.scatter(xy[0][ixs], xy[1][ixs], 0.15, velocs[ixs], vmin=-800, vmax=800, cmap='plasma')
+                    c = plt.colorbar(h3, ax=haxMOA)
+                    c.set_label('Along-track velocity (m/yr)')
+                except:
+                    pass
 
         outfile = plot_out_location + 'rgt' + rgt + '.' + beam + '_vs_measures_veloc' + str(velocity_number).zfill(
             2) + '.png'
